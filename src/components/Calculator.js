7eb536ab879d7d530afd95e984e5d12755c87880
jihaneh <jijih@hotmail.fr>
2022-02-08 16:04:13 +0100

    Refactor Calculator component using hook

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,57 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../logic/operate';
 import calculate from '../logic/calculate';
 import Keyboard from './Keyboard';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      result: 0,
-    };
-    this.calculObj = {};
-    this.handleClick = this.handleClick.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-  }
+const Calculator = () => {
+  const [calculObj, setCalculObj] = useState({});
+  const [result, setResult] = useState(0);
 
-  handleClick = (event) => {
-    this.calculObj = calculate(this.calculObj, event.target.innerText);
-    this.setState({
-      result: (this.calculObj.next === null && this.calculObj.operation === null)
-        ? this.calculObj.total : this.calculObj.next,
-    });
+  const handleClick = (event) => {
+    const newCalObj = calculate(calculObj, event.target.innerText);
+    setCalculObj(newCalObj);
+    setResult(newCalObj.next === null && newCalObj.operation === null
+      ? newCalObj.total : newCalObj.next);
   };
 
-  handleKeyDown = () => {};
+  const handleKeyDown = () => {};
 
-  render() {
-    const { result } = this.state;
-
-    return (
-      <div className="calculator">
-        <div className="result">{result}</div>
-        <Keyboard value="AC" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="+/-" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="%" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard classes="keyboard orange" value="÷" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="7" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="8" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="9" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard classes="keyboard orange" value="x" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="4" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="5" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="6" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard classes="keyboard orange" value="-" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="1" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="2" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="3" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard classes="keyboard orange" value="+" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard classes="zero" value="0" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard value="." clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-        <Keyboard classes="keyboard orange" value="=" clickCallback={this.handleClick} keydownCallback={this.handleKeyDown} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="calculator">
+      <div className="result">{result}</div>
+      <Keyboard value="AC" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="+/-" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="%" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard classes="keyboard orange" value="÷" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="7" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="8" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="9" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard classes="keyboard orange" value="x" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="4" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="5" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="6" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard classes="keyboard orange" value="-" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="1" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="2" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="3" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard classes="keyboard orange" value="+" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard classes="zero" value="0" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard value="." clickCallback={handleClick} keydownCallback={handleKeyDown} />
+      <Keyboard classes="keyboard orange" value="=" clickCallback={handleClick} keydownCallback={handleKeyDown} />
+    </div>
+  );
+};
 
 export default Calculator;
