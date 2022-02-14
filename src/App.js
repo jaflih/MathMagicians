a14943423ff657116d4ecc9a0cdfa1e1290a97e0
jihaneh <jijih@hotmail.fr>
2022-02-14 14:33:43 +0100

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Quote from './pages/Quote';
+import Home from './pages/Home';
+import NotMatch from './pages/NotMatch';
+
 import './App.css';
 
 export default class App extends React.Component {
@@ -9,6 +14,17 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <Calculator />;
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/quote" element={<Quote />} />
+            <Route path="*" element={<NotMatch />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    );
   }
 }
