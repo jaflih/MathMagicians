fdfd0940c91a2b9fa906ff95bd491389357c9ed1
jihaneh <jijih@hotmail.fr>
2022-02-14 15:44:57 +0100

    Fix github deploy issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import CalculatorContainer from './pages/CalculatorContainer';
 import Quote from './pages/Quote';
 import Home from './pages/Home';
@@ -16,14 +17,14 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route index element={<Home />} />
             <Route path="/calculator" element={<CalculatorContainer />} />
             <Route path="/quote" element={<Quote />} />
             <Route path="*" element={<NotMatch />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </>
     );
   }
