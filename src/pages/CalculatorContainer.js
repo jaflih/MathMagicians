455e8e6aed835d7b34c32d300d319742b2264cd5
jihaneh <jijih@hotmail.fr>
2022-02-14 16:36:04 +0100

    Update Calculator design and component

--- src/pages/CalculatorContainer.js
+++ src/pages/CalculatorContainer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Calculator from '../components/Calculator';
-import Navbar from '../components/Navbar';
+import Header from '../components/Header';
 
 const CalculatorContainer = () => (
-  <div>
-    <Navbar />
-    <Calculator />
-  </div>
+  <section>
+    <Header />
+    <div className="calcul">
+      <h2>Let&apos;s do some math!</h2>
+      <Calculator />
+    </div>
+  </section>
 );
 
 export default CalculatorContainer;
