0562ab45de2d01aa6e603e227e382484320fb502
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-16 16:54:19 +0200

    Add test for the calculator page: Manipulate the calculator

--- src/__test__/app_render.test.js
+++ src/__test__/app_render.test.js
@@ -37,4 +37,48 @@ describe('App', () => {
     expect(screen.queryByText("Let's do some math!")).toBeNull();
   });
 
+  test('Use the calculator Addition', () => {
+    render(<Calculator />);
+    userEvent.click(screen.queryByText('9'));
+    userEvent.click(screen.queryByText('+'));
+    userEvent.click(screen.queryByText('2'));
+    userEvent.click(screen.queryByText('='));
+    expect(screen.getByText('11')).toBeInTheDocument();
+  });
+
+  test('Use the calculator with Subtraction ', () => {
+    render(<Calculator />);
+    userEvent.click(screen.queryByText('95'));
+    userEvent.click(screen.queryByText('-'));
+    userEvent.click(screen.queryByText('2'));
+    userEvent.click(screen.queryByText('='));
+    expect(screen.getByText('93')).toBeInTheDocument();
+  });\
+
+  test('Use the calculator with Multiplication ', () => {
+    render(<Calculator />);
+    userEvent.click(screen.queryByText('4'));
+    userEvent.click(screen.queryByText('x'));
+    userEvent.click(screen.queryByText('3'));
+    userEvent.click(screen.queryByText('='));
+    expect(screen.getByText('12')).toBeInTheDocument();
+  });
+
+  test('Use the calculator with Division ', () => {
+    render(<Calculator />);
+    userEvent.click(screen.queryByText('16'));
+    userEvent.click(screen.queryByText('÷'));
+    userEvent.click(screen.queryByText('4'));
+    userEvent.click(screen.queryByText('='));
+    expect(screen.getByText('4')).toBeInTheDocument();
+  });
+
+  test('Use the calculator with Modulus ', () => {
+    render(<Calculator />);
+    userEvent.click(screen.queryByText('13'));
+    userEvent.click(screen.queryByText('%'));
+    userEvent.click(screen.queryByText('3'));
+    userEvent.click(screen.queryByText('='));
+    expect(screen.getByText('1')).toBeInTheDocument();
+  });
 });
