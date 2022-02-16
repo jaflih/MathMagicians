768290e988f77669d97fa5c87a19fd82fc264169
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-16 16:33:57 +0200

    Add test(render) for NotMatch, Quote, Calculator and Keyboard components

--- src/__test__/components.test.js
+++ src/__test__/components.test.js
@@ -3,6 +3,10 @@ import renderer from 'react-test-renderer';
 import CalculatorContainer from '../pages/CalculatorContainer';
 import Home from '../pages/Home';
 import App from '../App';
+import NotMatch from '../pages/NotMatch';
+import Quote from '../pages/Quote';
+import Calculator from '../components/Calculator';
+import Keyboard from '../components/Keyboard';
 
 it('renders correctly', () => {
   const tree = renderer.create(<App />).toJSON();
@@ -16,5 +20,22 @@ it('renders correctly', () => {
 
 it('renders correctly', () => {
   const tree = renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer.create(<NotMatch />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+it('renders correctly', () => {
+  const tree = renderer.create(<Quote />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+it('renders correctly', () => {
+  const tree = renderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+it('renders correctly', () => {
+  const tree = renderer.create(<Keyboard />).toJSON();
   expect(tree).toMatchSnapshot();
 });
